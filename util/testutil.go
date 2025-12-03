4d8ec0656dd3b1d909a145d5237cbe35c4a78a8a
Oliver Newman <orn688@gmail.com>
2025-12-03 01:10:42 -0500

    workflows: Add a 1-minute timeout to `go test`
    ... to make sure any sort of infinite loop doesn't cause tests to burn
    through GitHub workflow capacity too quickly.

--- util/testutil.go
+++ util/testutil.go
@@ -3,6 +3,8 @@ package util
 import "testing"
 
 func CheckAnswer(t *testing.T, impl func(string) (string, error), input, want string) {
+	t.Helper()
+
 	got, err := impl(input)
 	if err != nil {
 		t.Fatal(err)

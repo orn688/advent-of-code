e85ad92ca2d14a4f0092c7e9dd1a980e951cf667
Oliver Newman <orn688@gmail.com>
2025-12-03 18:37:59 -0500

    2025: Validate correctness of solution registry

--- 2025/main.go
+++ 2025/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
+	"reflect"
+	"runtime"
+	"strconv"
 	"strings"
 	"time"
 
@@ -13,10 +17,55 @@ import (
 	"github.com/orn688/advent-of-code/util"
 )
 
+const year = 2025
+
 var days = map[int][2]func(string) (string, error){
 	1: {day01.Part1, day01.Part2},
 	2: {day02.Part1, day02.Part2},
 	3: {day03.Part1, day03.Part2},
+}
+
+func init() {
+	// Go doesn't support dynamically importing the package and function for the
+	// given day and part, so we need to keep track of a mapping up above.
+	//
+	// Here we validate that the mapping is correct, i.e. that `dayX.PartY` is
+	// mapped to key X and at index Y-1. This helps catch copy-pasta when
+	// registering solutions for new days.
+	var errs []error
+	for day, solutions := range days {
+		for i, soln := range solutions {
+			part := i + 1
+			// e.g. "github.com/foo/bar/pkg.FunctionName"
+			absFuncName := runtime.FuncForPC(reflect.ValueOf(soln).Pointer()).Name()
+
+			parts := strings.Split(absFuncName, "/")
+			if parts[len(parts)-2] != strconv.Itoa(year) {
+				errs = append(errs, fmt.Errorf("wrong year for day %d part %d", day, part))
+			}
+			// e.g. pkg.FunctionName"
+			relFuncName := parts[len(parts)-1]
+
+			pkg, funcName, ok := strings.Cut(relFuncName, ".")
+			if !ok {
+				log.Panicf("failed to get package and function names")
+			}
+
+			if pkg != fmt.Sprintf("day%02d", day) {
+				errs = append(errs, fmt.Errorf("wrong package %q for day %d part %d", pkg, day, part))
+			}
+
+			if funcName != fmt.Sprintf("Part%d", part) {
+				errs = append(errs, fmt.Errorf("wrong function %q for day %d part %d", funcName, day, part))
+			}
+		}
+	}
+	for _, err := range errs {
+		fmt.Println("ERROR: " + err.Error())
+	}
+	if len(errs) > 0 {
+		os.Exit(1)
+	}
 }
 
 func main() {
@@ -42,7 +91,7 @@ func mainImpl(ctx context.Context) error {
 		return fmt.Errorf("no solution for day %d part %d", flags.Day, flags.Part)
 	}
 
-	input, err := util.FetchInput(ctx, 2025, flags.Day)
+	input, err := util.FetchInput(ctx, year, flags.Day)
 	if err != nil {
 		return err
 	}
